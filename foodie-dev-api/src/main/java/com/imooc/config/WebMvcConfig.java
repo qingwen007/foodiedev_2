27eee041ae43285a2b3ccf0f88fb6430a5ff8271
qwchen <1030679732@qq.com>
2021-02-06 17:02:44 +0800

    用户中心头像上传

--- foodie-dev-api/src/main/java/com/imooc/config/WebMvcConfig.java
+++ foodie-dev-api/src/main/java/com/imooc/config/WebMvcConfig.java
@@ -2,9 +2,10 @@ package com.imooc.config;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@Component
 public class WebMvcConfig implements WebMvcConfigurer {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder){return builder.build();}
